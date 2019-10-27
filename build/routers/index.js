bf54c5c26c34235f1995870d3196a4c00ae8bcb7
vanny96 <47816664+vanny96@users.noreply.github.com>
2019-10-27 11:42:02 +0100

    Delete index.js

--- build/routers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var router = express.Router();
-router.get('/', function (req, res) {
-    res.send({ message: "Hello WebApp" });
-});
-exports.default = router;
