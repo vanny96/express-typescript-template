857d04fc8fe73736532de6ee00761c495eac1b6a
vanny96 <47816664+vanny96@users.noreply.github.com>
2019-10-27 11:41:22 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express = require('express');
-import mongoose = require('mongoose');
 import cors = require('cors');
 
 import indexRouter from './routers/index';
@@ -7,9 +6,9 @@ import indexRouter from './routers/index';
 const app = express();
 
 // Database connection
-const mongoDbUrl = "mongodb://localhost:27017/projectName";
-mongoose.connect(mongoDbUrl, { useNewUrlParser: true });
-var db = mongoose.connection;
+// const mongoDbUrl = "mongodb://localhost:27017/projectName";
+// mongoose.connect(mongoDbUrl, { useNewUrlParser: true });
+// var db = mongoose.connection;
 
 // Configure app Cross Origin Resource Sharing
 app.use(cors());
